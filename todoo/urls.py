9f07a2e94255870791afc13b072a46a03734c42a
sarthakpatwa972@gmail.com <sarthakp972>
2025-06-12 13:58:52 +0530

    c

--- todoo/urls.py
+++ todoo/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include  # include is needed
+from django.shortcuts import redirect
+
 
 urlpatterns = [
+    path('', lambda request: redirect('/todo/login/')),
+
     path('admin/', admin.site.urls),
     path('todo/', include('todolist.urls')),  # correct usage
 ]
